ed076c51d25cce570f21c32468373a148515fd73
ggumbi-cs <cs.ggumbi@gmail.com>
2026-04-22 11:08:46 +0900

    Update chat_main.py

--- server/chat_main.py
+++ server/chat_main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
+
+clients = []
 
 @app.get("/")
 def root():
     return {"message": "chat server alive"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            for client in clients:
+                await client.send_text(data)
+
+    except WebSocketDisconnect:
+        clients.remove(websocket)

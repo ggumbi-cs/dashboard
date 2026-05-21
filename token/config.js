3d44313fc63379fcc7a356417243e7c045915a2e
ggumbi-cs <cs.ggumbi@gmail.com>
2026-05-21 11:15:29 +0900

    Add files via upload

--- token/config.js
+++ token/config.js
@@ -1,1 +1,4 @@
-
+// GitHub Gist 연동 설정
+// 이 파일은 .gitignore에 추가하여 깃허브에 올라가지 않게 하세요
+const GITHUB_TOKEN = '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';
+const GIST_ID      = '85f95642bd8bbc2635c6f7059a11dd3a';
